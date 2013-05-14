5d688fbfb9391e56226307ee84853ca1ddd8cec8
Ani <ani.ad.lib@gmail.com>
2013-05-14 17:27:13 +0300

    Add more test in TestBoards

--- TestMines/TestBoards.cs
+++ TestMines/TestBoards.cs
@@ -13,8 +13,10 @@ namespace TestMines
         private const int invalidWidthValue = -20;
         private const int validHeightValue = 10;
         private const int validWidthValue = 20;
-        private const int criticalBorderValue = 0;
-        
+        private const int criticalBorderValueZero = 0;
+        private const int criticalBorderValueOne = 1;
+
+        private Boards boards = new Boards(TestBoards.validHeightValue, TestBoards.validWidthValue);
        
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
@@ -34,7 +36,7 @@ namespace TestMines
         [ExpectedException(typeof(ArgumentException))]
         public void TestConstructorForCriticalHeightValue()
         {
-            Boards boards = new Boards(TestBoards.criticalBorderValue, TestBoards.validWidthValue);
+            Boards boards = new Boards(TestBoards.criticalBorderValueZero, TestBoards.validWidthValue);
             Assert.AreEqual(0, boards.Visible.GetLength(0));
         }
 
@@ -42,8 +44,43 @@ namespace TestMines
         [ExpectedException(typeof(ArgumentException))]
         public void TestConstructorForCriticalWidthValue()
         {
-            Boards boards = new Boards(TestBoards.validHeightValue, TestBoards.criticalBorderValue);
+            Boards boards = new Boards(TestBoards.validHeightValue, TestBoards.criticalBorderValueZero);
             Assert.AreEqual(0, boards.Visible.GetLength(1));
+        }
+
+        [TestMethod]
+        public void TestPropertyVisibleForGettingCorrectValue()
+        {           
+            Assert.IsTrue(boards.Visible.GetLength(0) == TestBoards.validHeightValue &&
+                          boards.Visible.GetLength(1) == TestBoards.validWidthValue);
+        }
+
+        [TestMethod]
+        public void TestPropertyHasMineForGettingCorrectValue()
+        {
+            Assert.IsTrue(boards.HasMine.GetLength(0) == TestBoards.validHeightValue &&
+                          boards.HasMine.GetLength(1) == TestBoards.validWidthValue);
+        }
+
+        [TestMethod]
+        public void TestPropertyIsShownForGettingCorrectValue()
+        {
+            Assert.IsTrue(boards.IsShown.GetLength(0) == TestBoards.validHeightValue &&
+                          boards.IsShown.GetLength(1) == TestBoards.validWidthValue);
+        }
+
+        [TestMethod]
+        public void TestPropertyNumberOfNeighbourMinesForGettingCorrectValue()
+        {
+            Assert.IsTrue(boards.NumberOfNeighbourMines.GetLength(0) == TestBoards.validHeightValue &&
+                          boards.NumberOfNeighbourMines.GetLength(1) == TestBoards.validWidthValue);
+        }
+
+        [TestMethod]
+        public void TestBoardsForHeightAndWidthEqualToOne()
+        {
+            Boards newBoards = new Boards(TestBoards.criticalBorderValueOne, TestBoards.criticalBorderValueOne);
+           
         }
     }
 }
