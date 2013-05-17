741229ae355c047356748ec7bb0772c54156ab8e
id-rachev <i.d.rachev@abv.bg>
2013-05-17 18:27:49 +0300

    changed the name of the main method class to MinesweeperGameEngine.cs

---
+++ TestMines/TestMinesweeperGameEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestMines
+{
+    [TestClass]
+    public class TestMinesweeperGameEngine
+    {
+        [TestMethod]
+        public void TestReadCommand()
+        {
+            string[] inputCommands = new string[]
+            {
+                " 3 1       ",
+                "   Flag 1 2 ",
+                " TOP  ",
+                "ResTart  ",
+                "   eXit ",
+                "  Some Invalid Command To Test ReadCommand Method "
+            };
+
+            string[] expectedCommands = new string[]
+            {
+                "3 1",
+                "flag 1 2",
+                "top",
+                "restart",
+                "exit",
+                "some invalid command to test readcommand method"
+            };
+
+            for (int i = 0; i < inputCommands.Length; i++)
+            {
+                using (StringReader strReader = new StringReader(inputCommands[i]))
+                {
+                    Console.SetIn(strReader);
+                    string actual = Mines.MinesweeperGameEngine.ReadCommand();
+                    string expected = expectedCommands[i];
+                    Assert.AreEqual(expected, actual);
+                }
+            }
+        }
+    }
+}
